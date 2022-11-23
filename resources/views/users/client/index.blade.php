5c91b2298cb45f5fefe33c7d261e4a885e994ac9
arbi pramana <arbipram@gmail.com>
2022-11-23 13:33:39 +0700

    udpate module income

--- resources/views/users/client/index.blade.php
+++ resources/views/users/client/index.blade.php
@@ -4,7 +4,6 @@
     <div class="container-fluid">
         <div class="page-titles">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Data Master</a></li>
                 <li class="breadcrumb-item active"><a href="#">Client</a></li>
             </ol>
         </div>
